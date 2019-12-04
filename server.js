911a750cb50407597db125afab3c597d049bf64b
abdellatif arara <abdo.arafa28@gmail.com>
2019-12-04 00:52:39 -0800

    make it better

--- server.js
+++ server.js
@@ -9,6 +9,12 @@ var dbconnection = require("./dbconnection/dbconnection");
 var path = require('path')
 var port = process.env.PORT || 4000
 app.set('port', (port));
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
+  next();
+});
 
 app.use(express.static(__dirname + '/frontend/dist'));
 app.use(
