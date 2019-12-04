1ab313657fffe2e5a5ca1929c5fa819162fd6e99
abdellatif arara <abdo.arafa28@gmail.com>
2019-12-04 00:47:37 -0800

    make it better

--- dbconnection/dbconnection.js
+++ dbconnection/dbconnection.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose')
 
 function dbconnection () { 
-return mongoose.connect(process.env.'mongodb+srv://abdo:qFErftZFTLUKj5rp@users-vymnu.mongodb.net/userdata?retryWrites=true&w=majority')
+return mongoose.connect('mongodb+srv://abdo:qFErftZFTLUKj5rp@users-vymnu.mongodb.net/userdata?retryWrites=true&w=majority')
 }
 
 
