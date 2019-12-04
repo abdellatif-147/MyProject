eab63ee1d739bb8839491e8ccfd203e0962b1211
abdellatif arara <abdo.arafa28@gmail.com>
2019-12-04 01:13:06 -0800

    make it better

--- dbconnection/dbconnection.js
+++ dbconnection/dbconnection.js
@@ -1,8 +1,7 @@
 var mongoose = require('mongoose')
 
 function dbconnection () { 
-return 
-mongoose.connect('mongodb+srv://abdo:qFErftZFTLUKj5rp@users-vymnu.mongodb.net/userdata?retryWrites=true&w=majority')
+return mongoose.connect('mongodb+srv://abdo:qFErftZFTLUKj5rp@users-vymnu.mongodb.net/userdata?retryWrites=true&w=majority')
 }
 
 
--- server.js
+++ server.js
@@ -8,17 +8,17 @@ var createApis = require("./Apis/userApi");
 var dbconnection = require("./dbconnection/dbconnection");
 var path = require('path')
 
-
+app.use(express.json())
 app.use(express.static(__dirname + '/frontend/dist'));
 app.use(
   session({
     genid: uuid,
-    secret: "secret",cookie: { maxAge: 60000 }
+    secret: "secret"
   })
 );
-app.use(cors());
-app.use(express.json())
-
+app.use(cors({
+credentials: true
+}));
 
 app.use(cookieparser());
 dbconnection();
